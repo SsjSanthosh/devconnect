a0608821af299319233a3450dbb829e9036cffe8
Santhosh <bgj.santhosh@gmail.com>
2020-02-03 18:06:40 +0530

    prebuilt

---
+++ client/build/precache-manifest.3aab300dc6c3b1e107aee2647ef3cc03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24998b1bde88581f2175ba47ca17bac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47541d6179e8466520bb",
+    "url": "/static/css/main.77e53140.chunk.css"
+  },
+  {
+    "revision": "2e167866228d6a9991d3",
+    "url": "/static/js/2.5f76b22f.chunk.js"
+  },
+  {
+    "revision": "5ec987632bc308bcd1222d7283dbdad9",
+    "url": "/static/js/2.5f76b22f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47541d6179e8466520bb",
+    "url": "/static/js/main.78e93949.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
